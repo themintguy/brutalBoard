6935541920a7bd65fdfa37551daa9248a6904284
Kranthi Kumar Banda <bandakranthi31@gmail.com>
2026-02-08 20:03:49 +0530

    ci fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,8 +14,7 @@ type Tile = {
 function App() {
   const [tiles, setTiles] = useState<Tile[]>([]);
   const [userId, setUserId] = useState<string | null>(null);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [remainingMs, setRemainingMs] = useState(0);
+  const [remainingMs] = useState(0);
 
 
   useEffect(() => {
